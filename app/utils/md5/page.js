07d0dfb42e3cbcdb7607c47981c962633eac29ea
Northwalker <northwalker28@gmail.com>
2023-09-27 00:02:51 +0800

    Add utils

---
+++ app/utils/md5/page.js
@@ -0,0 +1,81 @@
+'use client';
+import Script from 'next/script';
+import { useCallback, useRef, useState } from 'react';
+import formatBytes from '@/utils/formatBytes.js';
+
+export default function Page() {
+  const inputRef = useRef(null);
+  const [fileData, setFileData] = useState({});
+  const [fileMd5, setFileMd5] = useState('');
+
+  const handleOnChange = useCallback(() => {
+    console.log('handleOnChange', inputRef.current.files[0]);
+    const file = inputRef.current.files[0];
+
+    if (!file) {
+      console.log('Clear file...');
+      return setFileMd5('');
+    }
+
+    setFileData({ name: file.name, size: file.size, type: file.type });
+    // eslint-disable-next-line
+    const spark = new SparkMD5.ArrayBuffer();
+    const fileReader = new FileReader();
+    // const chunkSize = 2 * 1024 * 1024; // Read in chunks of 2MB
+    // const chunks = Math.ceil(file.size / chunkSize);
+    // let currentChunk = 0;
+    fileReader.readAsArrayBuffer(file);
+    fileReader.onload = (e) => {
+      console.log('FileReader finished loading');
+      spark.append(e.target.result); // Append array buffer
+      const computedMd5 = spark.end();
+      console.log('Computed hash', computedMd5);
+      setFileMd5(computedMd5);
+    };
+    fileReader.onerror = () => console.log('Oops, something went wrong.');
+  }, []);
+
+  return (
+    <>
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/spark-md5/3.0.2/spark-md5.min.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log('Script on load');
+        }}
+        onError={(e) => {
+          console.error('Script failed to load', e);
+        }}
+      />
+      <div className="relative mx-auto my-0 max-w-xl px-4 pb-0 pt-4 text-center text-lg text-black dark:text-white md:pt-32">
+        <input type="file" ref={inputRef} onChange={handleOnChange} />
+        <section className="mt-8">
+          {fileMd5 && (
+            <div className="border border-orange-300 p-4 text-left">
+              <div>
+                <div>
+                  <div className="flex flex-row">
+                    <div className="w-24">File Name:</div>
+                    <div>{fileData?.name}</div>
+                  </div>
+                  <div className="flex flex-row">
+                    <div className="w-24">File Type:</div>
+                    <div>{fileData?.type}</div>
+                  </div>
+                  <div className="flex flex-row">
+                    <div className="w-24">File Size:</div>
+                    <div>{formatBytes(fileData?.size)}</div>
+                  </div>
+                  <div className="mt-4 flex flex-row flex-wrap">
+                    <div className="w-36">Computed Hash:</div>
+                    <div className="break-all">{fileMd5}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </section>
+      </div>
+    </>
+  );
+}
---
+++ utils/formatBytes.js
@@ -0,0 +1,12 @@
+// formatBytes.js
+'use strict';
+export default function formatBytes(bytes, decimals) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024,
+    dm = decimals || 2,
+    sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+  return (
+    Number.parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+  );
+}
